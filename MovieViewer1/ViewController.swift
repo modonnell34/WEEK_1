4c1e82a414cfe628869058de0d37013ec9a7df36
modonnell34 <modonnell34@fordham.edu>
2016-01-25 16:49:24 -0500

    Initial Commit

---
+++ MovieViewer1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MovieViewer1
+//
+//  Created by Maura O'Donnell on 1/25/16.
+//  Copyright © 2016 Maura O'Donnell. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
